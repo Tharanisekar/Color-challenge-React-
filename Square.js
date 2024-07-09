2e0288c27ffb16e6237e350d34cc5e2def82a86d
Tharani <127316853+Tharanisekar@users.noreply.github.com>
2024-07-09 19:31:19 +0530

    Add files via upload

---
+++ Square.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Square =({colorValue, hexValue, isDarkText}) =>
+    {
+    return(
+        <section 
+        className="square"
+        style={{
+            background : colorValue,
+            color :isDarkText ? "#000" :"#FFF"
+        }}
+        >
+            <p>
+                {colorValue ? colorValue :"Empty Value"}
+            </p>
+            <p>
+                {hexValue ? hexValue :null}
+            </p>
+    
+        </section>
+    )
+}
+Square.defaultProps ={
+    colorValue :"Empty Color Value"
+}
+
+export default Square
